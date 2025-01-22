92d0063a05d2946ce4a21c054f7223e83da2ee07
dinodajin <djbrachio@gmail.com>
2025-01-22 14:44:04 +0900

    Fix minor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="footer__right">
-                    © 2023 webstoryboy<br />
+                    © 2025 webstoryboy<br />
                     이 사이트는 리액트를 이용하여 제작하였습니다.
                 </div>
             </div>
