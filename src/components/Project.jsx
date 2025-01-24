16d31b9725903eaa3b67a91c203e46721aefdaa6
dinodajin <djbrachio@gmail.com>
2025-01-24 23:20:10 +0900

    Project01

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -16,12 +16,11 @@ const Project = () => {
     const proText = [
         {
             num: "01",
-            title: "어워드에도 올라간 포트폴리오",
-            desc: "라마 디자인을 통해 자신의 스킬을 가장 멋지게 표현한 포트폴리오입니다. 가로 모드와 세로 모드는 매우 인상적이며 특히 리액트와 비트를 이용하여 제작한 것은 더욱 놀랍습니다. 이 사이트는 awwwards.com에도 인정받아 올라간 포트폴리오입니다. 확실히 그의 뛰어난 디자인 스킬과 기술력이 빛을 발휘한 결과물인 것 같습니다.",
+            title: "디깅소비러를 위한 새로운 미니가전 브랜드 LG MOA",
+            desc: "다양한 기능의 시리즈로 모을수록 확장되는 세계관을 보유",
             img: pro01,
-            code: "https://github.com/kimsangjunv1/-React-Portfolio",
-            view: "https://portfoliosj-react.netlify.app",
-            name: "김상* 포트폴리오",
+            code: "https://github.com/dinodajin/Appy_backend",
+            // view: "https://portfoliosj-react.netlify.app",
         },
         {
             num: "02",
