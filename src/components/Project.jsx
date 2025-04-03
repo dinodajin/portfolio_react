031ee52fabcfcf5d82251bce0218b4c1cbc01ff7
dinodajin <djbrachio@gmail.com>
2025-04-03 17:17:34 +0900

    Project01 수정완료

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 
-import pro01 from "../assets/img/about.jpg";
+import pro01 from "../assets/img/appy.png";
 import pro02 from "../assets/img/about.jpg";
 import pro03 from "../assets/img/about.jpg";
 import pro04 from "../assets/img/about.jpg";
 import pro05 from "../assets/img/about.jpg";
-import pro06 from "../assets/img/about.jpg";
-import pro07 from "../assets/img/about.jpg";
-import pro08 from "../assets/img/about.jpg";
-import pro09 from "../assets/img/about.jpg";
-import pro10 from "../assets/img/about.jpg";
 
 const Project = () => {
 
@@ -17,10 +12,12 @@ const Project = () => {
         {
             num: "01",
             title: "디깅소비러를 위한 새로운 미니가전 브랜드 LG MOA",
-            desc: "다양한 기능의 시리즈로 모을수록 확장되는 세계관을 보유",
+            desc: "‘디깅 가전’ 컨셉을 기획하고, 소비자가 제품과 상호작용하며 감성적인 만족감을 얻을 수 있도록 AI 기반 공감형 채팅 기능을 개발했습니다. 그 결과, 2030세대 120명을 대상으로 한 설문조사에서 브랜드 친숙도가 40% 증가하는 성과를 거두었습니다. 이 경험을 통해 데이터 분석을 기반으로 소비자 행동을 깊이 이해하는 법과, AI 및 IoT 기술을 활용한 사용자 경험 개선의 중요성을 배웠습니다.",
             img: pro01,
             code: "https://github.com/dinodajin/Appy_backend",
-            // view: "https://portfoliosj-react.netlify.app",
+            view: "https://github.com/dinodajin/Appy_backend",
+            view2: "https://github.com/dinodajin/Appy_frontend"
+
         },
         {
             num: "02",
@@ -58,51 +55,6 @@ const Project = () => {
             view: "https://junseungpark.github.io/portfolio/index3.html",
             name: "박준* 포트폴리오",
         },
-        {
-            num: "06",
-            title: "화려함의 정석 포트폴리오",
-            desc: "화려하고 세련된 디자인과 다채로운 색상이 사용된 포트폴리오는 사용자를 홀릴 듯한 시각적인 매력을 지니고 있습니다. 포트폴리오 내의 각 작품들은 디테일한 디자인과 탁월한 시각적 표현력을 갖추고 있어, 주인공의 뛰어난 예술적 감각을 느낄 수 있습니다. 화려한 애니메이션 효과와 부드러운 전환은 사이트를 더욱 생동감 있게 만들어주며, 사용자들에게 색다른 경험을 선사합니다.",
-            img: pro06,
-            code: "/",
-            view: "https://webstoryboy.github.io/port2023/portfolio-student/DavidYang/index.html",
-            name: "포트폴리오",
-        },
-        {
-            num: "07",
-            title: "패럴랙스 정석 포트폴리오",
-            desc: "마치 예술작품을 감상하는 듯한 환상적인 경험을 선사하는 포트폴리오입니다. 패럴랙스 스크롤링을 활용하여 구성된 사이트는 사용자들에게 독특하고 멋진 시각적 효과를 제공합니다. 배경과 움직이는 요소들이 조화롭게 어우러져, 사이트 전반에 걸쳐 깊이와 입체감을 느낄 수 있습니다. 스크롤에 따라 움직이는 요소들은 마치 세계를 탐험하는 듯한 느낌을 주며, 사용자들을 끌어들이는 매력적인 요소로 작용합니다.",
-            img: pro07,
-            code: "/",
-            view: "https://webstoryboy.github.io/port2023/portfolio-student/TaeyongLee/index.html",
-            name: "포트폴리오",
-        },
-        {
-            num: "08",
-            title: "트랜지션 포트폴리오",
-            desc: "화면 전환과 요소들의 흐름이 순조롭고 매끄러운 작품들로 가득한 포트폴리오 사이트입니다. 페이지 간의 트랜지션은 마치 이야기를 풀어내는 듯한 흥미진진한 경험을 선사합니다. 트랜지션 효과의 적절한 활용은 작품들을 보다 동적이고 생동감 있게 만들어줍니다. 각 페이지의 이동이 자연스럽고 사용자들이 원활하게 사이트를 탐색할 수 있도록 배려된 구성은 개발자의 디자인 능력을 잘 보여주는 특징입니다.",
-            img: pro08,
-            code: "/",
-            view: "https://webstoryboy.github.io/port2023/portfolio-student/HyunroKim/index.html",
-            name: "포트폴리오",
-        },
-        {
-            num: "09",
-            title: "스크롤의 정석 포트폴리오",
-            desc: "스크롤링을 활용하여 훌륭한 사용자 경험을 선사하는 포트폴리오 사이트입니다. 스크롤을 내리면서 작품들이 순차적으로 나타나고 효과적으로 전환되는 것은 마치 예술적인 이야기를 읽어나가는 듯한 느낌을 주며 사용자를 매료시킵니다. 스크롤의 움직임을 통해 작품들이 서서히 드러나고, 각 페이지 간의 전환은 자연스럽고 부드럽습니다. 이러한 트랜지션과 애니메이션들이 작품들의 내용과 테마를 더욱 강조해줍니다.",
-            img: pro09,
-            code: "/",
-            view: "https://webstoryboy.github.io/port2023/portfolio-student/ChoJaeHyung/index.html",
-            name: "포트폴리오",
-        },
-        {
-            num: "10",
-            title: "모던함의 정석 포트폴리오",
-            desc: "현대적이고 세련된 디자인으로 가득한 포트폴리오 사이트입니다. 모던한 느낌과 세심한 디테일이 조화를 이루며, 사용자에게 신선하고 품격 있는 경험을 제공합니다. 사이트의 디자인은 깔끔하고 단정하면서도 특유의 감성과 풍부한 표현력이 느껴집니다. 간결한 레이아웃과 모던한 색상 선택은 주인공의 디자인 감각을 잘 보여주는 특징입니다.",
-            img: pro10,
-            code: "/",
-            view: "https://webstoryboy.github.io/port2023/portfolio-student/JooHyeji/index.html",
-            name: "포트폴리오",
-        },
     ];
 
     return (
@@ -120,7 +72,8 @@ const Project = () => {
                             </a>
                             <h3 className="title">{project.title}</h3>
                             <p className="desc">{project.desc}</p>
-                            <a href={project.view} target="_blank" className="site" rel="noreferrer">사이트 보기</a>
+                            <a href={project.view} target="_blank" className="site" rel="noreferrer">GITHUB</a>
+                            <a href={project.view2} target="_blank" className="site" rel="noreferrer">GITHUB</a>
                         </article>
                     ))}
                 </div>
